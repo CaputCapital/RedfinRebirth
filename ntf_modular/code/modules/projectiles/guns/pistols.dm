740202ec0045f50c07d6c056414fad7a9fc7339a
CaputCapital <60798623+CaputCapital@users.noreply.github.com>
2026-08-21 01:27:53 -0700

    Initialize KZ-Refesh-2-Extra

---
+++ ntf_modular/code/modules/projectiles/guns/pistols.dm
@@ -0,0 +1,170 @@
+/obj/item/weapon/gun/pistol/xmdivider/ntc
+	name = "\improper NT/105 'Unity' Revolver"
+	desc = "NTC's special production replica of Intertech's one of a kind 'Divider' revolver, named Unity after reverse engineering a sample. Fires .357 and .357 Foxfire rounds, This model has no burst fire but it has greater stopping power than it's original. They indeed let it get into enemy hands."
+	icon = 'ntf_modular/icons/obj/items/guns/pistols.dmi'
+	icon_state = "nt105"
+	worn_icon_state = "nt105"
+	caliber = CALIBER_357 //codex
+	max_shells = 6
+	default_ammo_type = /obj/item/ammo_magazine/pistol/ntunity
+	allowed_ammo_types = list(/obj/item/ammo_magazine/pistol/xmdivider, /obj/item/ammo_magazine/pistol/xmdivider/ap, /obj/item/ammo_magazine/pistol/ntunity)
+	force = 8
+	attachable_allowed = list(
+		/obj/item/attachable/bayonet/converted,
+		/obj/item/attachable/flashlight,
+		/obj/item/attachable/compensator,
+		/obj/item/attachable/heavy_barrel,
+		/obj/item/attachable/extended_barrel,
+		/obj/item/attachable/lasersight,
+		/obj/item/attachable/lace,
+	)
+	attachable_offset = list("muzzle_x" = 32, "muzzle_y" = 20, "rail_x" = 17, "rail_y" = 22, "under_x" = 29, "under_y" = 15, "stock_x" = 10, "stock_y" = 18)
+	burst_amount = 1
+	//gonna adjust for no burst fire.
+	windup_delay = 0.2 SECONDS
+	fire_delay = 0.2 SECONDS
+	scatter_unwielded = 4
+	scatter = 1
+	damage_mult = 1.3
+	gun_firemode_list = list(GUN_FIREMODE_SEMIAUTO)
+	recoil = 1
+	recoil_unwielded = 5
+	//no red dot, holo sight instead.
+	accuracy_mult_unwielded = 0.8
+	accuracy_mult = 1
+	aim_speed_modifier = 0.50
+	holstered_underlay_icon = 'ntf_modular/icons/obj/items/storage/holster.dmi'
+
+//XM104 cylinder placed in pistols
+/obj/item/ammo_magazine/pistol/ntunity
+	name = "\improper NT105 'Unity' Foxfire cylinder (.357)"
+	desc = "CC/104 cylinder loaded with custom .357 armor-piercing incendiary rounds."
+	icon = 'ntf_modular/icons/obj/items/ammo/pistol.dmi' //same icon as parent so using that mag wont fuck the color illusion
+	default_ammo = /datum/ammo/bullet/revolver/heavy/foxfire
+	max_rounds = 6
+	caliber = CALIBER_357
+	icon_state = "nt105"
+	icon_state_mini = "nt105"
+	bonus_overlay = "nt105_fox"
+
+/datum/ammo/bullet/revolver/heavy/foxfire
+	name = "armor-piercing foxfire heavy revolver bullet"
+	handful_amount = 6
+	damage = 50
+	penetration = 15
+	sundering = 5
+	ammo_behavior_flags = AMMO_INCENDIARY|AMMO_BALLISTIC
+
+//mom i want a mateba, we got a mateba at home, mateba at home:
+/datum/ammo/bullet/revolver/heavy/foxfire/on_hit_mob(mob/target_mob, atom/movable/projectile/proj)
+    if(ishuman(target_mob))
+        staggerstun(target_mob, proj, paralyze = 0, stun = 1 SECONDS, stagger = 1 SECONDS, slowdown = 1, knockback = 1)
+    else
+        staggerstun(target_mob, proj, paralyze = 1 SECONDS, stagger = 1 SECONDS, slowdown = 1, knockback = 1)
+
+/obj/item/ammo_magazine/pistol/g22tranq
+	name = "\improper P-22 Custom tranq magazine (9mm)"
+	caliber = CALIBER_9X19_TRANQUILIZER
+	icon_state = "g22"
+	icon_state_mini = "mag_pistol_normal"
+	max_rounds = 12
+	default_ammo = /datum/ammo/bullet/pistol/tranq
+/obj/item/ammo_magazine/pistol/c99t
+	name = "\improper PK-9 tranq magazine (.22)"
+	default_ammo = /datum/ammo/bullet/pistol/tranq/weak
+	caliber = CALIBER_22LR
+	icon_state = "pk-9_tranq"
+	max_rounds = 8
+	icon_state_mini = "mag_pistol_green"
+
+/datum/ammo/bullet/pistol/tranq
+	name = "tranq bullet"
+	hud_state = "pistol_tranq"
+	armor_type = "bullet"
+	damage = 15
+	shell_speed = 3.3
+	shrapnel_chance = 0.2
+	var/inject_amount_min = 5
+	var/inject_amount_max = 8
+
+/datum/ammo/bullet/pistol/tranq/on_hit_mob(mob/target_mob, atom/movable/projectile/proj)
+	if(iscarbon(target_mob) && !isxeno(target_mob))
+		var/mob/living/carbon/carbon_victim = target_mob
+		carbon_victim.reagents.add_reagent(/datum/reagent/toxin/sleeptoxin, rand(inject_amount_min,inject_amount_max), no_overdose = TRUE)
+	else if(isxeno(target_mob))
+		var/mob/living/carbon/xenomorph/xtarg = target_mob
+		xtarg.use_stun_health(proj.damage * (xtarg.xeno_caste.max_health/200))
+
+/datum/ammo/bullet/pistol/tranq/weak
+	name = "weak tranq bullet"
+	damage = 10
+	accuracy = -10 //like usual c99 ammo
+	inject_amount_min = 3
+	inject_amount_max = 5
+
+/obj/item/weapon/gun/pistol/g22/tranq
+	name = "\improper P-22 custom pistol"
+	desc = "A 20th century military firearm customized for special forces use, fires chemical loaded bullets to take down enemies nonlethally. Must be cocked manually therefore has disgusting fire rate, but custom frame allows greater accuracy."
+	icon = 'ntf_modular/icons/obj/items/guns/pistols.dmi'
+	icon_state = "g22"
+	worn_icon_state = "g22"
+	fire_animation = null //it doesnt cycle itself.
+	cock_animation = "g22_fire"
+	cock_delay = 1 SECONDS
+	caliber = CALIBER_9X19_TRANQUILIZER //codex
+	load_method = MAGAZINE //codex
+	max_shells = 13
+	default_ammo_type = /obj/item/ammo_magazine/pistol/g22tranq
+	allowed_ammo_types = list(/obj/item/ammo_magazine/pistol/g22tranq, /obj/item/ammo_magazine/pistol/g22)
+	attachable_offset = list("muzzle_x" = 29, "muzzle_y" = 20,"rail_x" = 10, "rail_y" = 21, "under_x" = 21, "under_y" = 15, "stock_x" = 21, "stock_y" = 17)
+	attachable_allowed = list(
+		/obj/item/attachable/suppressor,
+		/obj/item/attachable/reddot,
+		/obj/item/attachable/flashlight,
+		/obj/item/attachable/compensator,
+		/obj/item/attachable/lasersight,
+		/obj/item/attachable/extended_barrel,
+		/obj/item/attachable/heavy_barrel,
+		/obj/item/attachable/flashlight/under,
+		/obj/item/attachable/gyro,
+		/obj/item/attachable/lace,
+		/obj/item/attachable/motiondetector/advanced/pocket/covert,
+	)
+	starting_attachment_types = list(
+		/obj/item/attachable/reddot,
+		/obj/item/attachable/lasersight,
+		/obj/item/attachable/suppressor,
+	)
+	reciever_flags = AMMO_RECIEVER_MAGAZINES|AMMO_RECIEVER_REQUIRES_UNIQUE_ACTION|AMMO_RECIEVER_UNIQUE_ACTION_LOCKS
+	cocked_message = "You rack the pistol"
+	cock_locked_message = "The pistol is loaded! Fire it first!"
+	gun_features_flags = GUN_CAN_POINTBLANK|GUN_AMMO_COUNTER
+	fire_delay = 0.7 SECONDS //manual cock anyway, meant to be able to not get obliterated up close too badly, unlike moonbeam.
+	accuracy_mult = 1.8
+	accuracy_mult_unwielded = 1.5
+	burst_amount = 1
+	akimbo_additional_delay = 0.9
+
+/obj/item/weapon/gun/pistol/m1911/custom/specops
+	name = "\improper P-1911SO custom pistol"
+	desc = "A handgun that has received an unholy amount of modifications. It seems to have been lovingly taken care of and passed down for generations. Lacks an auto magazine eject feature."
+	default_ammo_type = /obj/item/ammo_magazine/pistol/m1911/ap
+	attachable_allowed = list(
+		/obj/item/attachable/reddot,
+		/obj/item/attachable/flashlight,
+		/obj/item/attachable/lasersight,
+		/obj/item/attachable/extended_barrel,
+		/obj/item/attachable/suppressor,
+		/obj/item/attachable/quickfire,
+		/obj/item/attachable/lace,
+		/obj/item/attachable/buildasentry,
+		/obj/item/attachable/shoulder_mount,
+		/obj/item/attachable/motiondetector/advanced/pocket/covert,
+	)
+	starting_attachment_types = list(
+		/obj/item/attachable/reddot,
+		/obj/item/attachable/lasersight,
+		/obj/item/attachable/suppressor,
+	)
+	accuracy_mult = 1.15
+	accuracy_mult_unwielded = 0.95
---
+++ tgui/packages/tgui/interfaces/PlayerPreferences/BackgroundInformation.tsx
@@ -0,0 +1,320 @@
+import { useState } from 'react';
+import { Box, Button, Section, Stack, TextArea } from 'tgui-core/components';
+
+import { useBackend } from '../../backend';
+
+export const BackgroundInformation = (props) => {
+  const { act, data } = useBackend<BackgroundInformationData>();
+  const {
+    flavor_text,
+    med_record,
+    gen_record,
+    sec_record,
+    exploit_record,
+    xeno_desc,
+    profile_pic,
+    nsfwprofile_pic,
+    xenoprofile_pic,
+  } = data;
+  const [characterDesc, setCharacterDesc] = useState(flavor_text);
+  const [medicalDesc, setMedicalDesc] = useState(med_record);
+  const [employmentDesc, setEmploymentDesc] = useState(gen_record);
+  const [securityDesc, setSecurityDesc] = useState(sec_record);
+  const [exploitsDesc, setExploitsDesc] = useState(exploit_record);
+  const [xenoDesc, setXenoDesc] = useState(xeno_desc);
+  const [profilePic, setProfilePic] = useState(profile_pic);
+  const [nsfwprofilePic, setNSFWProfilePic] = useState(nsfwprofile_pic);
+  const [xenoprofilePic, setXenoProfilePic] = useState(xenoprofile_pic);
+  return (
+    <Section title="Background information">
+      <Section
+        title="Character Description"
+        buttons={
+          <Box>
+            <Button
+              icon="save"
+              disabled={characterDesc === flavor_text}
+              onClick={() => act('flavor_text', { characterDesc })}
+            >
+              Save
+            </Button>
+            <Button icon="times" onClick={() => setCharacterDesc(flavor_text)}>
+              Reset
+            </Button>
+          </Box>
+        }
+      >
+        <TextArea
+          expensive
+          key="character"
+          fluid
+          height="200px"
+          maxLength={12000}
+          value={characterDesc}
+          onChange={setCharacterDesc}
+        />
+      </Section>
+      <Section
+        title="Xenomorph Description"
+        buttons={
+          <Box>
+            <Button
+              icon="save"
+              disabled={xenoDesc === xeno_desc}
+              onClick={() => act('xeno_desc', { xenoDesc })}
+            >
+              Save
+            </Button>
+            <Button icon="times" onClick={() => setXenoDesc(xeno_desc)}>
+              Reset
+            </Button>
+          </Box>
+        }
+      >
+        <TextArea
+          expensive
+          key="xeno"
+          fluid
+          height="200px"
+          maxLength={12000}
+          value={xenoDesc}
+          onChange={(value) => setXenoDesc(value)}
+        />
+      </Section>
+      <Stack>
+        <Stack.Item grow>
+          <Section
+            title="Medical Records"
+            buttons={
+              <Box>
+                <Button
+                  icon="save"
+                  disabled={medicalDesc === med_record}
+                  onClick={() => act('med_record', { medicalDesc })}
+                >
+                  Save
+                </Button>
+                <Button icon="times" onClick={() => setMedicalDesc(med_record)}>
+                  Reset
+                </Button>
+              </Box>
+            }
+          >
+            <TextArea
+              fluid
+              height="100px"
+              expensive
+              maxLength={1024}
+              value={medicalDesc}
+              onChange={setMedicalDesc}
+            />
+          </Section>
+        </Stack.Item>
+        <Stack.Item grow>
+          <Section
+            title="Employment Records"
+            buttons={
+              <Box>
+                <Button
+                  icon="save"
+                  disabled={employmentDesc === gen_record}
+                  onClick={() => act('gen_record', { employmentDesc })}
+                >
+                  Save
+                </Button>
+                <Button
+                  icon="times"
+                  onClick={() => setEmploymentDesc(gen_record)}
+                >
+                  Reset
+                </Button>
+              </Box>
+            }
+          >
+            <TextArea
+              fluid
+              height="100px"
+              maxLength={1024}
+              value={employmentDesc}
+              expensive
+              onChange={setEmploymentDesc}
+            />
+          </Section>
+        </Stack.Item>
+      </Stack>
+      <Stack>
+        <Stack.Item grow>
+          <Section
+            title="Security Records"
+            buttons={
+              <Box>
+                <Button
+                  icon="save"
+                  disabled={securityDesc === sec_record}
+                  onClick={() => act('sec_record', { securityDesc })}
+                >
+                  Save
+                </Button>
+                <Button
+                  icon="times"
+                  onClick={() => setSecurityDesc(sec_record)}
+                >
+                  Reset
+                </Button>
+              </Box>
+            }
+          >
+            <TextArea
+              fluid
+              height="100px"
+              maxLength={1024}
+              value={securityDesc}
+              expensive
+              onChange={setSecurityDesc}
+            />
+          </Section>
+        </Stack.Item>
+        <Stack.Item grow>
+          <Section
+            title="Exploit Records"
+            buttons={
+              <Box>
+                <Button
+                  icon="save"
+                  disabled={exploitsDesc === exploit_record}
+                  onClick={() => act('exploit_record', { exploitsDesc })}
+                >
+                  Save
+                </Button>
+                <Button
+                  icon="times"
+                  onClick={() => setExploitsDesc(exploit_record)}
+                >
+                  Reset
+                </Button>
+              </Box>
+            }
+          >
+            <TextArea
+              fluid
+              height="100px"
+              maxLength={1024}
+              value={exploitsDesc}
+              expensive
+              onChange={setExploitsDesc}
+            />
+          </Section>
+        </Stack.Item>
+      </Stack>
+      Set picture to &lsquo;!clear&rsquo; to clear
+      <hr />
+      <Stack>
+        <Stack.Item grow>
+          <Section
+            title="Human Picture"
+            buttons={
+              <Box>
+                <Button
+                  icon="save"
+                  disabled={profilePic === profile_pic}
+                  onClick={() => act('profile_pic', { profilePic })}
+                >
+                  Save
+                </Button>
+                <Button icon="times" onClick={() => setProfilePic(profile_pic)}>
+                  Reset
+                </Button>
+              </Box>
+            }
+          >
+            <TextArea
+              expensive
+              fluid
+              height="100px"
+              maxLength={2048}
+              value={profilePic}
+              onChange={(value) => setProfilePic(value)}
+            />
+            {profile_pic ? (
+              <img src={profile_pic} width={350} height={400} />
+            ) : (
+              ''
+            )}
+          </Section>
+        </Stack.Item>
+        <Stack.Item grow>
+          <Section
+            title="Human Nakie Picture"
+            buttons={
+              <Box>
+                <Button
+                  icon="save"
+                  disabled={nsfwprofilePic === nsfwprofile_pic}
+                  onClick={() => act('nsfwprofile_pic', { nsfwprofilePic })}
+                >
+                  Save
+                </Button>
+                <Button
+                  icon="times"
+                  onClick={() => setProfilePic(nsfwprofile_pic)}
+                >
+                  Reset
+                </Button>
+              </Box>
+            }
+          >
+            <TextArea
+              expensive
+              fluid
+              height="100px"
+              maxLength={2048}
+              value={nsfwprofilePic}
+              onChange={(value) => setNSFWProfilePic(value)}
+            />
+            {nsfwprofile_pic ? (
+              <img src={nsfwprofile_pic} width={350} height={400} />
+            ) : (
+              ''
+            )}
+          </Section>
+        </Stack.Item>
+        <Stack.Item grow>
+          <Section
+            title="Xeno Profile Picture"
+            buttons={
+              <Box>
+                <Button
+                  icon="save"
+                  disabled={xenoprofilePic === xenoprofile_pic}
+                  onClick={() => act('xenoprofile_pic', { xenoprofilePic })}
+                >
+                  Save
+                </Button>
+                <Button
+                  icon="times"
+                  onClick={() => setXenoProfilePic(xenoprofile_pic)}
+                >
+                  Reset
+                </Button>
+              </Box>
+            }
+          >
+            <TextArea
+              expensive
+              fluid
+              height="100px"
+              maxLength={2048}
+              value={xenoprofilePic}
+              onChange={(value) => setXenoProfilePic(value)}
+            />
+            {xenoprofile_pic ? (
+              <img src={xenoprofile_pic} width={350} height={400} />
+            ) : (
+              ''
+            )}
+          </Section>
+        </Stack.Item>
+      </Stack>
+    </Section>
+  );
+};
